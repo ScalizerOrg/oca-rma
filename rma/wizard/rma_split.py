4188af2230f843c0b2117365dd9aac8ae3e6b3d6
Morgane Goujon <morgane@scalizer.fr>
2026-07-22 15:22:39 +0200

    [FIX] migration of sql_contrainst to V19 odoo

--- rma/wizard/rma_split.py
+++ rma/wizard/rma_split.py
@@ -24,13 +24,10 @@ class RmaReSplitWizard(models.TransientModel):
         required=True,
     )
 
-    _sql_constraints = [
-        (
-            "check_product_uom_qty_positive",
-            "CHECK(product_uom_qty > 0)",
-            "Quantity must be greater than 0.",
-        ),
-    ]
+    _check_product_uom_qty_positive = models.Constraint(
+        "CHECK(product_uom_qty > 0)",
+        "Quantity must be greater than 0.",
+    )
 
     @api.model
     def fields_get(self, allfields=None, attributes=None):
